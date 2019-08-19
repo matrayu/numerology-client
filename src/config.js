f28193e763af283ec7c9f76e1be177236328391c
matrayu <northridge614@gmail.com>
2019-08-18 22:06:21 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   /* API_ENDPOINT: "http://localhost:8000/api", */
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+  /* API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT, */
+  API_ENDPOINT: 'https://ancient-cove-47706.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY
 }
